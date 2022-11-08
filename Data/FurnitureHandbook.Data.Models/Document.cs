0500611f942ee30bb1d4f7f54601c20acbe179d1
simonasta <simonasta@abv.bg>
2022-11-08 15:00:12 +0200

    Add new Migration UpdateDocumentModel,create Category ById view

--- Data/FurnitureHandbook.Data.Models/Document.cs
+++ Data/FurnitureHandbook.Data.Models/Document.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
@@ -10,9 +11,14 @@
 
     public class Document : BaseDeletableModel<int>
     {
+        [Required]
+        public string Name { get; set; }
+
         public int CategoryId { get; set; }
 
         public virtual Category Category { get; set; }
+
+        public int Size { get; set; }
 
         public string FileUrl { get; set; }
 
---
+++ Data/FurnitureHandbook.Data/Migrations/20221108125811_UpdateDocumentModel.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FurnitureHandbook.Data.Migrations
+{
+    public partial class UpdateDocumentModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Documents",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Size",
+                table: "Documents",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Documents");
+
+            migrationBuilder.DropColumn(
+                name: "Size",
+                table: "Documents");
+        }
+    }
+}
