a0ed731d3647ab476b0df824579baa562c61b63a
simonasta <simonasta@abv.bg>
2022-12-08 23:30:30 +0200

    Create ClientMockRepository and write tests

---
+++ Tests/FurnitureHandbook.Services.Data.Tests/ClientServiceTests.cs
@@ -0,0 +1,61 @@
+﻿namespace FurnitureHandbook.Services.Data.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using FurnitureHandbook.Data.Common.Repositories;
+    using FurnitureHandbook.Data.Models;
+    using FurnitureHandbook.Services.Data.Clients;
+    using FurnitureHandbook.Services.Data.Tests.Mocks;
+    using FurnitureHandbook.Web.ViewModels.Clients;
+    using Microsoft.AspNetCore.Http;
+    using Moq;
+    using Xunit;
+
+    public class ClientServiceTests
+    {
+        private readonly Mock<IDeletableEntityRepository<Client>> mockRepo;
+        private readonly ClientsService service;
+
+        public ClientServiceTests()
+        {
+            this.mockRepo = ClientMockRepository.GetClientMockRepo();
+            this.service = new ClientsService(this.mockRepo.Object);
+        }
+
+        [Fact]
+        public async Task CreateClientTest()
+        {
+            var client = new CreateClientInputModel
+            {
+                FullName = "Симона Милчева",
+                PhoneNumber = "0895988888",
+                Address = "гр.Пловдив, ул.Славееви гори 19",
+            };
+
+            await this.service.CreateAsync(client);
+
+            var count = this.mockRepo.Object.All().Count();
+            var name = this.mockRepo.Object.All().Skip(2).First().FullName;
+            Assert.Equal("Симона Милчева", name);
+
+            Assert.True(count == 3);
+        }
+
+        [Fact]
+        public async Task CreateClientExceptionTest()
+        {
+            var client = new CreateClientInputModel
+            {
+                FullName = "Александра Файн",
+                PhoneNumber = "0893636659",
+                Address = "бул.Коматевско шосе 35 , гр.Пловдив",
+            };
+
+            await Assert.ThrowsAsync<System.Exception>(() => this.service.CreateAsync(client));
+        }
+    }
+}
---
+++ Tests/FurnitureHandbook.Services.Data.Tests/Mocks/ClientMockRepository.cs
@@ -0,0 +1,49 @@
+﻿namespace FurnitureHandbook.Services.Data.Tests.Mocks
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using FurnitureHandbook.Data.Common.Repositories;
+    using FurnitureHandbook.Data.Models;
+    using Moq;
+
+    public class ClientMockRepository
+    {
+        public static Mock<IDeletableEntityRepository<Client>> GetClientMockRepo()
+        {
+            var mockRepo = new Mock<IDeletableEntityRepository<Client>>();
+
+            var list = new List<Client>()
+            {
+                new Client
+                {
+                    FullName = "Александра Файн",
+                    PhoneNumber = "0893636659",
+                    Address = "бул.Коматевско шосе 35 , гр.Пловдив",
+                },
+                new Client
+                {
+                    FullName = "Гергана Кирева",
+                    PhoneNumber = "0889864840",
+                    Address = "ж.к Тракия блок 108 вход Г , гр.Пловдив",
+                },
+            };
+
+            mockRepo.Setup(r => r.All()).Returns(list.AsQueryable());
+
+            mockRepo.Setup(r => r.AllAsNoTracking()).Returns(list.Where(x => x.IsDeleted == false).AsQueryable());
+
+            mockRepo.Setup(r => r.AllAsNoTrackingWithDeleted()).Returns(list.AsQueryable());
+
+            mockRepo.Setup(r => r.Delete(It.IsAny<Client>()))
+                                 .Callback((Client client) => list.Remove(client));
+            mockRepo.Setup(r => r.AddAsync(It.IsAny<Client>()))
+                                 .Callback((Client client) => list.Add(client));
+
+            return mockRepo;
+        }
+    }
+}
